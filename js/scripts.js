31e98fc56cee3b20412aa8a599f89277f66fa381
ConceptualComet <zaniest.coeval_2a@icloud.com>
2026-03-24 17:10:57 -0400

    Update scripts.js added colophon definition

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,7 @@ const contentPositions = {
   'shrines-content': { top: 0.24, left: 0.55, width: 0.18, height: 0.50 },
   'curriculum-content': { top: 0.20, left: 0.54, width: 0.22, height: 0.54 },
   'colophon-content': { top: 0.12, left: 0.54, width: 0.22, height: 0.30 },
+  'colophon-definition-content': { top: 0.54, left: 0.54, width: 0.10, height: 0.30 },
   'now-content': { top: 0.6, left: 0.225, width: 0.18, height: 0.185 },
   'about-content': { top: 0.20, left: 0.20, width: 0.10, height: 0.20 },
   'links-content': { top: 0.54, left: 0.27, width: 0.10, height: 0.20 }
