c694a9be3ad08c95b52608ce77e8707c8eb4887c
ConceptualComet <zaniest.coeval_2a@icloud.com>
2026-03-24 11:06:58 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -163,8 +163,8 @@ for (const [className, pos] of Object.entries(contentPositions)) {
  // Position music player
    const musicPlayer = document.querySelector('.music-player');
      if (musicPlayer) {
-      musicPlayer.style.top = (offsetY + renderedHeight * 0.80) + 'px';
-      musicPlayer.style.left = (offsetX + renderedWidth * 0.13) + 'px';
+      musicPlayer.style.top = (offsetY + renderedHeight * 0.85) + 'px';
+      musicPlayer.style.left = (offsetX + renderedWidth * 0.16) + 'px';
      }
 }
 
